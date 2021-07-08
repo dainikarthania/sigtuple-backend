a988b099acb2f9eb125cacf249a1d4c9c1b6dbc5
Dainik <dainik.arthania@gmail.com>
2021-07-09 01:07:48 +0530

    backend added

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '88cb61274ddaa01341b6e40a84cd2799'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '346df58c-3ace-4ae1-b385-0e7d958e1051'
+};
